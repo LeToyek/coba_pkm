78684dff78fcefd213487d3ba42bb071f537ee54
LeToyek <maulanatoyek@gmail.com>
2022-03-08 22:44:58 +0700

    finished 1 page

--- lib/pages/Login_page.dart
+++ lib/pages/Login_page.dart
@@ -8,26 +8,82 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: SingleChildScrollView(
           child: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Register',
-              style: Theme.of(context)
-                  .textTheme
-                  .headline4
-                  ?.copyWith(color: Colors.orange, fontWeight: FontWeight.bold),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 64,
+                ),
+                Text(
+                  'Register',
+                  style: Theme.of(context).textTheme.headline4?.copyWith(
+                      color: Colors.orange, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Create your new account',
+                  style: TextStyle(fontSize: 16),
+                ),
+                SizedBox(
+                  height: 32,
+                ),
+                LoginField(
+                  prefIcon: Icon(Icons.person),
+                  hint: "Full Name",
+                ),
+                LoginField(
+                  prefIcon: Icon(Icons.mail),
+                  hint: "Email",
+                ),
+                LoginField(
+                  prefIcon: Icon(Icons.lock),
+                  hint: "Password",
+                ),
+                LoginField(
+                  prefIcon: Icon(Icons.lock),
+                  hint: "Confirm Password",
+                ),
+                Text(
+                  "By signing you agree to our Terms of use and previous notice",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(),
+                ),
+                SizedBox(
+                  height: 64,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(24)),
+                  padding: EdgeInsets.all(16),
+                  width: double.infinity,
+                  child: Center(
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Already have account ? '),
+                    Text(
+                      'Sign in',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ],
+                )
+              ],
             ),
-            Text(
-              'Create your new account',
-              style: TextStyle(fontSize: 16),
-            ),
-            LoginField(
-              prefIcon: Icon(Icons.person),
-            ),
-            LoginField(prefIcon: Icon(Icons.mail))
-          ],
+          ),
         ),
       )),
     );
--- lib/widgets/Login_field.dart
+++ lib/widgets/Login_field.dart
@@ -2,13 +2,29 @@ import 'package:flutter/material.dart';
 
 class LoginField extends StatelessWidget {
   Icon prefIcon;
-  LoginField({required this.prefIcon});
+  String hint;
+  LoginField({required this.prefIcon, required this.hint});
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration:
-          InputDecoration(prefixIcon: prefIcon, border: InputBorder.none),
+    return Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+              isDense: true,
+              prefixIcon: prefIcon,
+              filled: true,
+              fillColor: Colors.grey[100],
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none),
+              hintText: hint,
+              hintStyle: TextStyle(fontSize: 16)),
+        ),
+        SizedBox(
+          height: 16,
+        )
+      ],
     );
   }
 }
